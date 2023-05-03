b834bbc983b3eb8119810dd72ce8cf9ec38b8c75
ademrosier <fraadem@outlook.dk>
2023-05-03 21:53:09 +0200

    student create method

--- ZealandBook/ZealandBook/Services/SQLService/SQLServiceStudent.cs
+++ ZealandBook/ZealandBook/Services/SQLService/SQLServiceStudent.cs
@@ -1,6 +1,25 @@
-﻿namespace ZealandBook.Services.SQLService
+﻿using ZealandBook.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ZealandBook.Services.SQLService
 {
     public class SQLServiceStudent
     {
+        private static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ZeabookDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static void CreateStudent(Student student)
+        {
+            string query = $"INSERT into Teacher(StudentId, StudentName, StudentEmail) Values(@StudentId, @StudentName, @StudentEmail)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("StudentId", student.StudentId);
+                    command.Parameters.AddWithValue("@StudentName", student.StudentName);
+                    command.Parameters.AddWithValue("@StudenEmail", student.StudentEmail);
+                    int affectedRows = command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
