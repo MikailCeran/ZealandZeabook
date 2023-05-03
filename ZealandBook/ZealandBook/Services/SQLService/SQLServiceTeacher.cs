214d04cf45f51a6f17a339d07b2062929abb1877
ademrosier <fraadem@outlook.dk>
2023-05-03 20:46:38 +0200

    teacher create method

--- ZealandBook/ZealandBook/Services/SQLService/SQLServiceTeacher.cs
+++ ZealandBook/ZealandBook/Services/SQLService/SQLServiceTeacher.cs
@@ -1,6 +1,26 @@
-﻿namespace ZealandBook.Services.SQLService
+﻿using ZealandBook.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ZealandBook.Services.SQLService
 {
     public class SQLServiceTeacher
     {
+        private static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ZeabookDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static void CreateTeacher(Teacher teacher)
+        {
+            string query = $"INSERT into Teacher(TeacherID, TeacherName, TeacherEmail, Admin) Values(@TeacherID, @TeacherName, @TeacherEmail, @Admin)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("TeacherID", teacher.TeacherID);
+                    command.Parameters.AddWithValue("@TeacherName", teacher.TeacherName);
+                    command.Parameters.AddWithValue("@TeacherEmail", teacher.TeacherEmail);
+                    command.Parameters.AddWithValue("@Admin", teacher.Admin);
+                    int affectedRows = command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
