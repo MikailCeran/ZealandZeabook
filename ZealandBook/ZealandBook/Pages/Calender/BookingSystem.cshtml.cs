3b9c09bb0adc666377b6207a3eb34f87476f7243
mrmahir <mahoekinci@gmail.com>
2023-05-28 01:15:37 +0200

    Calendar part 2

--- ZealandBook/ZealandBook/Pages/Calender/BookingSystem.cshtml.cs
+++ ZealandBook/ZealandBook/Pages/Calender/BookingSystem.cshtml.cs
@@ -1,43 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.Collections.Generic;
+using ZealandBook.Models;
+using ZealandBook.Services.Interface;
 
-namespace ZealandBook.Pages.Calender
+namespace ZealandBook
 {
     public class BookingSystemModel : PageModel
     {
-        // Properties
-        public List<int> AvailableRoomIds { get; set; }
 
-        // GET request handler
-        public void OnGet()
+        [BindProperty]
+        public Booking booking { get; set; } = new Booking();
+
+        private IBookingService bookingService;
+        public BookingSystemModel(IBookingService bookingService)
         {
-            // Initialize the available room IDs as an empty list
-            AvailableRoomIds = new List<int>();
+            this.bookingService = bookingService;
         }
 
-        // POST request handler
-        public IActionResult OnPost(DateTime specificDate, TimeSpan specificTimeFrom, TimeSpan specificTimeTo)
-        {
-            // Call the GetAllAvailableRoomIds method to retrieve the available room IDs
-            AvailableRoomIds = GetAllAvailableRoomIds(specificDate, specificTimeFrom, specificTimeTo);
+        public IEnumerable<int> bookings { get; set; }
 
-            // Return the same page with the updated results
-            return Page();
+        public void OnGet(DateTime specificDate, TimeSpan specificTimeFrom, TimeSpan specificTimeTo)
+        {
+            bookings = bookingService.GetAllAvailableRoomIds(specificDate, specificTimeFrom, specificTimeTo);
+
         }
 
-        // Function to get all available room IDs
-        public List<int> GetAllAvailableRoomIds(DateTime specificDate, TimeSpan specificTimeFrom, TimeSpan specificTimeTo)
-        {
-            // Implement your logic to retrieve the available room IDs here
-            // You can use the existing SQL query or any other method you prefer
 
-            List<int> roomIds = new List<int>();
+        //public List<int> GetAllAvailableRoomIds(DateTime specificDate, TimeSpan specificTimeFrom, TimeSpan specificTimeTo);
 
-            // Add your code here to retrieve the available room IDs
 
-            return roomIds;
-        }
+
+       
     }
 }
